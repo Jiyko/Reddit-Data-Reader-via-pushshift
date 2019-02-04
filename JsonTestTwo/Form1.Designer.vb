b93753d9bbdc04cc686c35184025ec9fa874dea5
DESKTOP-A19DI78\Jiyko <WTapps@Hotmail.com>
2019-02-04 16:26:27 +0000

    Add project files.

---
+++ JsonTestTwo/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SubReddit = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MainText = New System.Windows.Forms.RichTextBox()
+        Me.StartDate = New System.Windows.Forms.DateTimePicker()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.EndDate = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.FileNameBox = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.NumberToCheck = New System.Windows.Forms.TextBox()
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SubReddit
+        '
+        Me.SubReddit.Location = New System.Drawing.Point(12, 30)
+        Me.SubReddit.Name = "SubReddit"
+        Me.SubReddit.Size = New System.Drawing.Size(73, 20)
+        Me.SubReddit.TabIndex = 0
+        Me.SubReddit.Text = "FortniteBR"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "SubReddit"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.Button1.Location = New System.Drawing.Point(12, 333)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(203, 105)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Do The Thing"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 304)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(203, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Start Date"
+        '
+        'MainText
+        '
+        Me.MainText.Location = New System.Drawing.Point(12, 190)
+        Me.MainText.Name = "MainText"
+        Me.MainText.ReadOnly = True
+        Me.MainText.Size = New System.Drawing.Size(203, 108)
+        Me.MainText.TabIndex = 6
+        Me.MainText.Text = ""
+        '
+        'StartDate
+        '
+        Me.StartDate.Location = New System.Drawing.Point(12, 119)
+        Me.StartDate.Name = "StartDate"
+        Me.StartDate.Size = New System.Drawing.Size(203, 20)
+        Me.StartDate.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 142)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "End Date"
+        '
+        'EndDate
+        '
+        Me.EndDate.Location = New System.Drawing.Point(12, 158)
+        Me.EndDate.Name = "EndDate"
+        Me.EndDate.Size = New System.Drawing.Size(203, 20)
+        Me.EndDate.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(130, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "File Name To Use"
+        '
+        'FileNameBox
+        '
+        Me.FileNameBox.Location = New System.Drawing.Point(130, 79)
+        Me.FileNameBox.Name = "FileNameBox"
+        Me.FileNameBox.Size = New System.Drawing.Size(85, 20)
+        Me.FileNameBox.TabIndex = 10
+        Me.FileNameBox.Text = "Pre"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(100, 13)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Number "
+        '
+        'NumberToCheck
+        '
+        Me.NumberToCheck.Location = New System.Drawing.Point(99, 30)
+        Me.NumberToCheck.Name = "NumberToCheck"
+        Me.NumberToCheck.Size = New System.Drawing.Size(116, 20)
+        Me.NumberToCheck.TabIndex = 12
+        Me.NumberToCheck.Text = "300"
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 56)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 44)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Select File Location"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 448)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.NumberToCheck)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.FileNameBox)
+        Me.Controls.Add(Me.EndDate)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.StartDate)
+        Me.Controls.Add(Me.MainText)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SubReddit)
+        Me.Name = "Form1"
+        Me.Text = "MRG"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SubReddit As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MainText As RichTextBox
+    Friend WithEvents StartDate As DateTimePicker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents EndDate As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents FileNameBox As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents NumberToCheck As TextBox
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ JsonTestTwo/Form1.vb
@@ -0,0 +1,207 @@
+﻿Imports System
+Imports System.Net
+Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim sURL As String
+        Dim FirstRunURL As String
+        Dim SecondRunURL As String
+        Dim Final As String
+
+        Dim sMonth As String
+        Dim sDay As String
+        Dim eMonth As String
+        Dim eDay As String
+        Dim sDate As Date
+        Dim eDate As Date
+
+
+        Dim bodyTracker As String
+
+        Dim FinalStartDate As String
+        Dim FinalEndDate As String
+        Dim FinalSize As String
+        Dim FinalTags As String
+
+        Dim FinalBodyTags As String
+
+        Dim ValidLines As Int64
+        Dim TotalLines As Int64
+        Dim ValidPoster As Int64
+        Dim TotalPoster As Int64
+
+        'Declare the IO Stuff
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter("D:\RedditRips\Test\" + FileNameBox.Text() + ".txt", True)
+
+
+        'Get the latest stats
+        Dim statsFile As System.IO.StreamReader
+        statsFile = My.Computer.FileSystem.OpenTextFileReader("D:\RedditRips\Test\" + FileNameBox.Text() + "Stats.txt")
+        ValidLines = Convert.ToInt32(statsFile.ReadLine())
+        TotalLines = Convert.ToInt32(statsFile.ReadLine())
+        ValidPoster = Convert.ToInt32(statsFile.ReadLine())
+        TotalPoster = Convert.ToInt32(statsFile.ReadLine())
+
+        statsFile.Close()
+
+        Dim statsFileW As System.IO.StreamWriter
+        statsFileW = My.Computer.FileSystem.OpenTextFileWriter("D:\RedditRips\Test\" + FileNameBox.Text() + "Stats.txt", False)
+
+        sURL = "https://api.pushshift.io/reddit/search/comment/?subreddit="
+        sURL = sURL + (SubReddit.Text())
+
+
+        'Start Date Junk
+        sDate = StartDate.Value()
+        sMonth = (sDate.Month).ToString
+        sDay = (sDate.Day).ToString
+
+        If (sMonth.Length < 2) Then
+            sMonth = "0" + sMonth
+        End If
+
+        If (sDay.Length < 2) Then
+            sDay = "0" + sDay
+        End If
+
+        FinalStartDate = "&after=" + (sDate.Year).ToString + "-" + sMonth + "-" + sDay
+
+
+        'Start Date Junk
+        eDate = EndDate.Value()
+        eMonth = (eDate.Month).ToString
+        eDay = (eDate.Day).ToString
+
+        If (eMonth.Length < 2) Then
+            eMonth = "0" + eMonth
+        End If
+
+        If (eDay.Length < 2) Then
+            eDay = "0" + eDay
+        End If
+
+        FinalEndDate = "&before=" + (eDate.Year).ToString + "-" + eMonth + "-" + eDay
+
+        ProgressBar1.Maximum = CInt(NumberToCheck.Text())
+
+        FinalSize = "&size=" + NumberToCheck.Text()
+        FinalTags = "&filter=author,score"
+
+        FinalBodyTags = "&filter=body"
+
+        FirstRunURL = sURL + FinalStartDate + FinalEndDate + FinalSize + FinalTags
+        SecondRunURL = sURL + FinalStartDate + FinalEndDate + FinalSize + "&filter=body&&author="
+
+        MainText.Text = MainText.Text + FirstRunURL
+
+        Dim wrGETURL As WebRequest
+        wrGETURL = WebRequest.Create(FirstRunURL)
+
+        Dim objStream As Stream
+        objStream = wrGETURL.GetResponse.GetResponseStream()
+
+        Dim objReader As New StreamReader(objStream)
+
+        Dim sLine As String = ""
+        Dim i As Integer = 0
+
+
+
+        Dim wrGEInnerURL As WebRequest
+
+        Dim objInnerStream As Stream
+
+        Dim sInnerLine As String = ""
+        Dim j As Integer = 0
+        Dim k As Integer = 0
+
+
+        Do While Not sLine Is Nothing
+            i += 1
+            sLine = objReader.ReadLine
+            If Not sLine Is Nothing Then
+
+                If sLine.Contains("author") Then
+                    TotalPoster = TotalPoster + 1
+                    ProgressBar1.Value = ProgressBar1.Value + 1
+
+                    Console.WriteLine("{0}:{1}", i, sLine)
+
+                    sLine = sLine.Remove((sLine.Length) - 2)
+                    sLine = sLine.Substring(23)
+
+                    Console.WriteLine(sLine)
+
+                    Final = SecondRunURL + sLine
+                    wrGEInnerURL = WebRequest.Create(Final)
+                    objInnerStream = wrGEInnerURL.GetResponse.GetResponseStream()
+
+                    Dim objInnerReader As New StreamReader(objInnerStream)
+
+                    sInnerLine = ""
+                    j = 0
+                    k = 0
+
+                    bodyTracker = ""
+
+                    Do While Not sInnerLine Is Nothing
+                        j += 1
+
+
+                        If sInnerLine.Contains("body") Then
+
+                            If Not sInnerLine.Contains("https") Then
+
+                                If Not sInnerLine.Contains("removed") Then
+
+                                    If Not sInnerLine.Contains("I am a bot") Then
+
+                                        If Not sInnerLine.Contains("remove") Then
+                                            sInnerLine = sInnerLine.Remove((sInnerLine.Length) - 1)
+                                            sInnerLine = sInnerLine.Substring(21)
+                                            sInnerLine.Trim()
+
+                                            Console.WriteLine(sInnerLine)
+                                            bodyTracker = bodyTracker + vbCrLf + sInnerLine
+
+                                            TotalLines = TotalLines + 1
+                                            k += 1
+                                        End If
+                                    End If
+                                End If
+                            End If
+                        End If
+
+
+                        sInnerLine = objInnerReader.ReadLine
+
+                    Loop
+                    If k >= 3 Then
+                        If bodyTracker.Length >= 750 Then
+                            file.WriteLine(bodyTracker)
+                            ValidPoster = ValidPoster + 1
+                            ValidLines = ValidLines + k
+                        End If
+                    End If
+                End If
+            End If
+        Loop
+
+        statsFileW.WriteLine(ValidLines)
+        statsFileW.WriteLine(TotalLines)
+        statsFileW.WriteLine(ValidPoster)
+        statsFileW.WriteLine(TotalPoster)
+
+        file.Close()
+        statsFileW.Close()
+        ProgressBar1.Value = 0
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
